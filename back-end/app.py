30ce214eb66d9d6dbb06d999fb9f19201ad5c6e0
Gaabrielbatista <ggabrielbattista@gmail.com>
2025-11-03 14:27:40 -0300

    Update

--- back-end/app.py
+++ back-end/app.py
@@ -1,23 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
-cors = CORS(app)
+CORS(app)
 
-@app.route("/login", methods=["POST"])
-def login():
-    # 1️⃣ Receber os dados JSON enviados pelo front
-    dados = request.get_json()
-    usuario = dados.get('usuario')
-    senha = dados.get('senha')
-
-    # 2️⃣ Fazer alguma verificação (exemplo simples)
-    if usuario == 'admin' and senha == '1234':
-        # 3️⃣ Retornar um JSON dizendo que deu certo
-        return jsonify({'sucesso': True})
-    else:
-        # 4️⃣ Retornar um JSON dizendo que deu errado
-        return jsonify({'sucesso': False})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def conectar():
+    conexao = mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="backend_user",
+        password="senha1234",
+        database="projeto_teste"
+    )
+    return conexao
