a3dc6413f4c3652c688379b1c2459b8197e5ba53
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-02-05 11:55:39 -0300

    feat: config inicial

--- back-end/app.py
+++ back-end/app.py
@@ -1,16 +1,5 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
-
-def conectar():
-    conexao = mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="backend_user",
-        password="senha1234",
-        database="projeto_teste"
-    )
-    return conexao
